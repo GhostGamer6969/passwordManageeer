3e7176fb2749675a78b436c7cd3a2ff0c6b5862f
GhostGamer6969 <arambhmani@Hotmail.com>
2024-03-02 12:34:22 +0530

    env update and modules fix
    npm install ethers@latest installed and
    contract address added

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -7,29 +7,30 @@ require('dotenv').config({ path: __dirname + '/.env' })
 
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: "0.8.11",
   networks:{
-    local:{
-      url: "HTTP://127.0.0.1:8545",
-      "chainId":1337,
-      accounts: ["0x9a2698927295e87505cb94dc31fac99f3d5b3a9599a8d2ba3eaf94190efcff8d"]
+    //   local:{
+    //     url: "HTTP://127.0.0.1:8545",
+    //     "chainId":1337,
+    //     accounts: ["0x9a2698927295e87505cb94dc31fac99f3d5b3a9599a8d2ba3eaf94190efcff8d"]
+    // },
+    hardhat: {},
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/Pn0wndYD8wb0FLdtoj_YvZx-sFC2bspd",
+      accounts: [process.env.POLYGON_MUMBAI_WALLET_KEY || ""]
+    },
+    // matic: {
+    //   url: "https://rpc-mainnet.maticvigil.com",
+    //   // accounts: [process.env.privateKey]
+    //   accounts: [""]
+    // }
   },
-  mumbai: {
-    url: "https://polygon-mumbai.infura.io/v3/4458cf4d1689497b9a38b1d6bbf05e78",
-    accounts: [process.env.POLYGON_MUMBAI_WALLET_KEY || ""]
-  },
-  // matic: {
-  //   url: "https://rpc-mainnet.maticvigil.com",
-  //   // accounts: [process.env.privateKey]
-  //   accounts: [""]
+  // etherscan:{
+  //   apiKey: {
+  //     polygon: process.env.POLYGONSCAN_KEY || "",
+  //     polygonMumbai: process.env.POLYGONSCAN_KEY || ""
+  //   }
   // }
-  },
-  etherscan:{
-    apiKey: {
-      polygon: process.env.POLYGONSCAN_KEY || "",
-      polygonMumbai: process.env.POLYGONSCAN_KEY || ""
-    }
-  }
 };
 
 export default config;
